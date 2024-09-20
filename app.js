24c081ea5dc6a19c367403d4af34eb88deb70a79
Joseph <joseph8778@gmail.com>
2024-09-19 21:00:41 -0500

    Added querySelector and getElementID in JS.

---
+++ app.js
@@ -0,0 +1,6 @@
+// First way of accessing an element
+console.log(document.querySelector('#title'))
+
+// Second way of accessing an element (Without a hash)
+
+console.log(document.getElementById('title'))
